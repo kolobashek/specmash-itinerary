6c546413eb93ef2e6400bff2d2e1fe839681ed6e
kolobashek <kolobashek@gmail.com>
2023-08-04 19:18:47 +0700

    ts.config fixes. graphql resolve error

--- App.tsx
+++ App.tsx
@@ -1,11 +1,8 @@
-import React from "react";
+import * as React from "react";
 import { observer } from "mobx-react-lite";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
-import {
-  createStackNavigator,
-  StackNavigationProp,
-} from '@react-navigation/stack';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import store from './src/store'
 import {
@@ -16,7 +13,7 @@ import {
   ContragentsScreen,
 } from './src/components'
 
-const Stack = createStackNavigator<RootStackParamList>()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 const REGISTER_SCREEN = 'Register'
 const LOGIN_SCREEN = 'Login'
 const INFO_SCREEN = 'Info'
@@ -29,7 +26,7 @@ export interface RootStackParamList {
   [key: string]: undefined | { userId: string }
 }
 
-type NavigationProps = StackNavigationProp<RootStackParamList>
+// type NavigationProps = StackNavigationProp<RootStackParamList>
 // Проверяем авторизацию пользователя
 const isAuthorized = store.getUserAuthorized()
 
@@ -42,17 +39,14 @@ const App = observer(() => {
   return (
     <SafeAreaProvider>
       <Router>
-        <NavigationContainer>
+        <BrowserRouter>
           <Stack.Navigator>
             <Stack.Screen name={LOGIN_SCREEN} component={LoginScreen} />
             <Stack.Screen name={REGISTER_SCREEN} component={RegisterScreen} />
             <Stack.Screen name={INFO_SCREEN} component={InfoScreen} />
-            <Stack.Screen
-              name={CONTRAGENTS_SCREEN}
-              component={ContragentsScreen}
-            />
+            <Stack.Screen name={CONTRAGENTS_SCREEN} component={ContragentsScreen} />
           </Stack.Navigator>
-        </NavigationContainer>
+        </BrowserRouter>
       </Router>
     </SafeAreaProvider>
   )
